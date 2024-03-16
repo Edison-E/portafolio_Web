f91251ad5d06f7da6e250a36624c4657ebd7dd0c
Edison-E <enrikvc2002@gmail.com>
2024-03-16 17:50:05 +0100

    Version_3 Portafolio

--- src/js/botonNocturnoLuz.js
+++ src/js/botonNocturnoLuz.js
@@ -1,17 +1,17 @@
 $("#cambiar").click(() => {
     var cambio = $("#cambiar").html();
-    if (cambio === "☼") {
+    if (cambio === "☽") {
         $("#cambiar").empty();
         $("#cambiar").css("text-align", "right");
-        $("#cambiar").html("☽");
+        $("#cambiar").html("☼");
         $("body").css("background-color", "black");
         $(".contenido_skills").css("background-image", "url(../../img/fondoSkills.png)")
         $(".barra_nav").css("color", "white");
         $(".nombre").css("color", "white");
-    } else if (cambio === "☽") {
+    } else if (cambio === "☼") {
         $("#cambiar").empty();
         $("#cambiar").css("text-align", "left");
-        $("#cambiar").html("☼");
+        $("#cambiar").html("☽");
         $("body").css("background-color", "white");
         $(".contenido_secundario div p").css("color","black")
         $(".contenido_skills").css("background-image", "url(../../img/fondoSKILL.png)")
